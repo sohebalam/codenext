b77957839831be0916657cab84a4f2e1961d9bd8
soheb alam <sohebalam@hotmail.com>
2021-12-04 08:58:39 +0000

    first

---
+++ controllers/userCont.js
@@ -0,0 +1,24 @@
+import User from "../models/userModel"
+import connectDB from "../connectDB"
+
+export const Register = async (user) => {
+  console.log("cont", user)
+
+  connectDB()
+
+  const userExists = await User.findOne({ socialId: user.id })
+
+  if (userExists) return
+
+  //   if (user.password) {
+  //     var salt = bcrypt.genSaltSync(10)
+  //     var hashPassword = bcrypt.hashSync(user.password, salt)
+  //   }
+
+  const registereduser = await User.create({
+    socialId: user.id,
+    name: user.name,
+    email: user.email,
+    // password: hashPassword || "",
+  })
+}
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import TwitterProvider from "next-auth/providers/twitter"
+import { Register } from "../../../controllers/userCont"
 
 export default NextAuth({
-  //   session: {
-  //     jwt: true,
-  //   },
   providers: [
     // OAuth authentication providers
     GoogleProvider({
@@ -17,16 +16,37 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+    }),
   ],
   //   pages: {
   //     signIn: "/login",
   //   },
   // SQL or MongoDB database (or leave empty)
-  //   database: process.env.DB,
-  //   callbacks: {
-  //     session: async (session, user) => {
-  //       session.userId = user.sub
-  //       return Promise.resolve(session)
-  //     },
-  //   },
+  database: process.env.DB,
+
+  session: {
+    jwt: true,
+  },
+  secret: process.env.SECRET,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      return token
+    },
+    async session({ session, token }) {
+      session.user.id = token.sub
+
+      if (session) {
+        Register(session.user)
+      }
+
+      return session
+    },
+  },
 })
